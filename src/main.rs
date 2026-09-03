44590913d65bdda5d0c91d763fcde67e329ca531
Cearzyk <adascesarz134@gmail.con>
2026-09-03 22:23:04 +0200

    Modularizing the code into functions and cleaning up the code.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,34 @@
-use std::{error::Error, fs::File, io::Read};
-
 //use buttplug::{
 //    ButtplugClient,
 //    connector::ButtplugRemoteClientConnector,
 //    device::{ClientDeviceCommandValue, ClientDeviceOutputCommand},
 //    serializer::ButtplugClientJSONSerializer,
 //};
-use reqwest::{Certificate, ClientBuilder};
+
+use reqwest::Client;
+use std::{error::Error, fs::File, io::Read};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let riot_id = "Cesarzyk#6088";
+async fn main() -> Result<(), Box<dyn Error>> {
+    let client = create_client()?;
+    let player_name = get_player_name(&client).await?;
+    get_player_score(&client, player_name).await?;
+    Ok(())
+}
+
+fn create_client() -> Result<Client, Box<dyn Error>> {
     // Certificate handling
     let mut buf = Vec::new();
     File::open("riotgames.pem")?.read_to_end(&mut buf)?;
     let cert = reqwest::Certificate::from_pem(&buf)?;
-    let client = reqwest::Client::builder()
+    let client: Client = reqwest::Client::builder()
         .add_root_certificate(cert)
         .use_rustls_tls()
         .build()?;
-    let player_name = client
-        .get("https://127.0.0.1:2999/liveclientdata/activeplayername")
-        .send()
-        .await?
-        .text()
-        .await?;
+    Ok(client)
+}
+async fn get_player_score(client: &Client, player_name: String) -> Result<String, reqwest::Error> {
+    //doing the acutal request
     let body = client
         .get(format!(
             "https://127.0.0.1:2999/liveclientdata/playerscores?riotId={}",
@@ -34,7 +38,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?
         .text()
         .await?;
-    println!("body = {body}");
-    println!("https://127.0.0.1:2999/liveclientdata/playerscores?riotId={player_name}");
-    Ok(())
+    Ok(body)
+}
+
+async fn get_player_name(client: &Client) -> Result<String, reqwest::Error> {
+    //getting the player name
+    let player_name = client
+        .get("https://127.0.0.1:2999/liveclientdata/activeplayername")
+        .send()
+        .await?
+        .text()
+        .await?;
+    Ok(player_name)
 }
